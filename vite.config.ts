c06cc5690ddc0630ec0334776ecdd85bfbbe2bf0
Okamich <sporer1@gmail.com>
2025-10-08 10:48:25 +0000

    Finaly! CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path' // 🔹 ДОБАВЛЕНО
+import tailwindcss from "@tailwindcss/vite"; // Импортируем плагин
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    tailwindcss(), // Добавляем плагин в массив
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // 🔹 ДОБАВЛЕНО
